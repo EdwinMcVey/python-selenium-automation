266455c985cb4029810fd92b17cd4e08f3756ce3
EdwinMcVey <edmcvey5376@gmail.com>
2021-12-06 16:57:35 -0600

    Lesson_2_amazon_signin_screen

---
+++ features/tests/amazon_signin_logo.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome("../chromedriver.exe")
+
+driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
+
+actual_text = driver.find_element(By.XPATH, "//a[@class='a-link-nav-icon']/i").text
+
+driver.quit()
+
